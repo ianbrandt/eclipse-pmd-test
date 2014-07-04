a69f9cd9b55bbdf24b0b45301e52f4206641cb56
Ian Brandt <ian@ianbrandt.com>
2014-07-04 11:33:39 -0700

    Initial drop.

---
+++ module-a/src/test/java/com/ianbrandt/pmd/eclipse/test/AppTest.java
@@ -0,0 +1,13 @@
+package com.ianbrandt.pmd.eclipse.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
